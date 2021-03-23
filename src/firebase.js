a52eea11d516b4cfaef48d5722b4244e0b7d7cef
Dhiraj3118 <61940283+Dhiraj3118@users.noreply.github.com>
2021-03-23 15:22:42 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDQCnWeL6BuIACZzkF_8XEAo73oy69zciA",
-    authDomain: "react-todo-app-2be32.firebaseapp.com",
-    projectId: "react-todo-app-2be32",
-    storageBucket: "react-todo-app-2be32.appspot.com",
-    messagingSenderId: "511920548226",
-    appId: "1:511920548226:web:03970f77b05c75e32e079b",
-    measurementId: "G-8MS9VJ3KPS"
+    apiKey: "API KEY",
+    authDomain: "AUTH DOMAIN",
+    projectId: "PROJECT ID",
+    storageBucket: "STORAGE BUCKET",
+    messagingSenderId: "MESSAGING SENDER ID",
+    appId: "APP ID",
+    measurementId: "MEASUREMENT ID"
 })
 
 const db = firebaseApp.firestore();
